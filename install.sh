e9409bffe8694178e95f6a3d47565cb28ba43df9
bob-crutchley <bob.crutchley@gmail.com>
2019-03-13 10:57:38 +0000

    updated the install script so that it install jenkins as well now

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+#####################
+# Jenkins Install
+
+# check if a jenkins user already exists
+if ! cat /etc/passwd | grep jenkins; then
+	# add the jenkins user
+	sudo useradd -m -s /bin/bash jenkins
+fi
+# check if the sudoers file has jenkins in it already
+if ! sudo cat /etc/sudoers | grep jenkins; then 
+	# make jenkins a sudo user
+	echo 'jenkins ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo
+fi
+# download the latest jenkins.war to the jenkins home folder
+sudo wget https://updates.jenkins-ci.org/latest/jenkins.war -o /home/jenkins/jenkins.war
+# make sure jenkins owns the jenkins.war file
+sudo chown jenkins:jenkins /home/jenkins/jenkins.war
+# install the jenkins service script
+sudo cp jenkins.service /etc/systemd/system/jenkins.service
+# reload all of the systemd service scripts
+sudo systemctl daemon-reload
+# start the jenkins service
+sudo systemctl restart jenkins
+
+#####################
+# Spring Install
+
 # create a spring user
 # - make sure the user has a home folder (-m)
 # - set the default shell to bash (-s)
@@ -12,3 +39,4 @@ sudo apt install -y maven
 sudo cp spring.service /etc/systemd/system/spring.service
 # reload all of the systemd service scripts
 sudo systemctl daemon-reload
+
